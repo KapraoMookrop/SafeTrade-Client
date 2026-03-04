549febeed6c5b89c971733fcd71ccec1b048f576
pitakarn99 <pitakarn_s@fissionsoft.co.th>
2026-03-04 17:34:32 +0700

    nav

--- src/app/component/navbar/navber.ts
+++ src/app/component/navbar/navber.ts
@@ -1,12 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule, NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AppService } from '../../API/AppService';
 import Swal from 'sweetalert2';
-import { App } from '../../app';
 import { AppStateService } from '../../core/AppStateService';
 import { LoadingService } from '../../core/LoadingService';
+import { AuthService } from '../../core/AuthService';
 
 @Component({
   selector: 'app-navbar',
@@ -14,7 +14,7 @@ import { LoadingService } from '../../core/LoadingService';
   providers: [],
   templateUrl: './navber.html',
 })
-export class Navbar {
+export class Navbar implements OnInit {
   dialogLoginVisible = false;
   dialogSignupVisible = false;
   dialogTranVisible = false;
@@ -22,14 +22,10 @@ export class Navbar {
   tron_address?: string;
   username?: string;
 
-  constructor(private stateService: AppStateService, private loadindService: LoadingService) { }
+  constructor(private stateService: AppStateService, private loadingService: LoadingService, private authAppService: AuthService ) { }
 
   ngOnInit() {
-    this.loadindService.show();
-  }
-
-  ngDoCheck() {
-
+    
   }
 
 }
