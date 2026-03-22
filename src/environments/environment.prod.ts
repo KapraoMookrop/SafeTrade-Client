7d784af3a0cdc31cd7755ead4aca18d07cf4565e
Chatchawan <chatchawan_t@fissionsoft.co.th>
2026-03-22 17:15:29 +0700

    chat change link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://safe-trade-server.vercel.app/api',
-  socketUrl: 'https://safe-trade-socket-server.vercel.app'
+  socketUrl: 'https://safetrade-socket.onrender.com'
 };
